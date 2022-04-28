1957c8a0770cd0e5c6ae7a6d002f43a5a6fc7b40
incoverlet <incoverlet@gmail.com>
2022-04-28 23:22:24 +0900

    -- cooking new 4/28 --

---
+++ app/src/main/java/com/example/cooking/menu_list.kt
@@ -0,0 +1,16 @@
+package com.example.cooking
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+
+class menu_list : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_list)
+
+    }
+}
+
